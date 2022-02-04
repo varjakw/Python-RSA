b694c592500b9e9752134efa26634de811ea6cb9
varjakw <varjakwolfe2@protonmail.com>
2022-02-04 19:58:05 +0000

    divided into functions and removed redundant code

--- RSA.java
+++ RSA.java
@@ -1,11 +1,27 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.math.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.swing.*;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.Base64;
 
 class RSA {
-    public static void main(String args) {
+    public static void main() throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
         //generate RSA key pair
+        KeyPair pair = generateKP();
+        PrivateKey privateKey = pair.getPrivate();
+        PublicKey publicKey = pair.getPublic();
+
+        String message = JOptionPane.showInputDialog ("What is your message?");
+
+        String encryptedMessage = encryptMessage(publicKey, message);
+
+
+    }
+
+    public static KeyPair generateKP(){
         KeyPairGenerator kpGen;
         try {
             kpGen = KeyPairGenerator.getInstance("RSA");
@@ -13,66 +29,20 @@ class RSA {
             throw new RuntimeException(e);
         }
         kpGen.initialize(2048); //keysize
-        KeyPair pair = kpGen.generateKeyPair();
-
-        PrivateKey privateKey = pair.getPrivate();
-        PublicKey publicKey = pair.getPublic();
-
-
-
-
-        int p, q, n, z, d = 0, e, i;
-
-        // The number to be encrypted and decrypted
-        int msg = 12;
-        double c;
-        BigInteger msgback;
-
-        // 1st prime number p
-        p = 3;
-
-        // 2nd prime number q
-        q = 11;
-        n = p * q;
-        z = (p - 1) * (q - 1);
-        System.out.println("the value of z = " + z);
-
-        for (e = 2; e < z; e++) {
-
-            // e is for public key exponent
-            if (gcd(e, z) == 1) {
-                break;
-            }
-        }
-        System.out.println("the value of e = " + e);
-        for (i = 0; i <= 9; i++) {
-            int x = 1 + (i * z);
-
-            // d is for private key exponent
-            if (x % e == 0) {
-                d = x / e;
-                break;
-            }
-        }
-        System.out.println("the value of d = " + d);
-        c = (Math.pow(msg, e)) % n;
-        System.out.println("Encrypted message is : " + c);
-
-        // converting int value of n to BigInteger
-        BigInteger N = BigInteger.valueOf(n);
-
-        // converting float value of c to BigInteger
-        BigInteger C = BigDecimal.valueOf(c).toBigInteger();
-        msgback = (C.pow(d)).mod(N);
-        System.out.println("Decrypted message is : "
-                + msgback);
+        return kpGen.generateKeyPair();
     }
 
-    static int gcd(int e, int z)
-    {
-        if (e == 0)
-            return z;
-        else
-            return gcd(z % e, e);
+    public static String encryptMessage(PublicKey publicKey, String message) throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        //a Cipher object for encryption with our public key
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        //now we encrypt the message:
+        //change message into bytes
+        byte[] secretMessageBytes = message.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptedMessageBytes = encryptCipher.doFinal(secretMessageBytes);
+
+        //encode with base64 alphabet for readability in storage (like in a DB)
+        return Base64.getEncoder().encodeToString(encryptedMessageBytes);
     }
 }
